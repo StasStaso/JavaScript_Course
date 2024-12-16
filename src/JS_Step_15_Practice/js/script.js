1bbc29795a24a3a5e24ff81b76bbc6572fdac63f
StasStaso <xctacx5@gmail.com>
2024-12-16 17:21:21 +0200

    js-step-15

---
+++ src/JS_Step_15_Practice/js/script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const movieDB = {
+    movies: [
+        "Логан",
+        "Лига справедливости",
+        "Ла-ла лэнд",
+        "Одержимость",
+        "Скотт Пилигрим против..."
+    ]
+};
+
+const adv = document.querySelectorAll('.promo__adv img');
+
+adv.forEach(element => {
+   element.remove(); 
+});
+
