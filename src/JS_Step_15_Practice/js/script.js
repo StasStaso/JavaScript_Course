5d2c9d77a682b9cfb331cc4ccb9909b891d024d2
StasoStas <xctacx5@gmail.com>
2024-12-16 19:26:29 +0200

    js-step-15-modify-1

--- src/JS_Step_15_Practice/js/script.js
+++ src/JS_Step_15_Practice/js/script.js
@@ -1,18 +1,36 @@
-'use strict';
+"use strict";
 
 const movieDB = {
-    movies: [
-        "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
-    ]
+  movies: [
+    "Логан",
+    "Лига справедливости",
+    "Ла-ла лэнд",
+    "Одержимость",
+    "Скотт Пилигрим против...",
+  ],
 };
 
-const adv = document.querySelectorAll('.promo__adv img');
+const adv = document.querySelectorAll(".promo__adv img"),
+  poster = document.querySelector(".promo__bg"),
+  movieList = document.querySelector(".promo__interactive-list"),
+  genre = poster.querySelector(".promo__genre");
 
-adv.forEach(element => {
-   element.remove(); 
+adv.forEach((element) => {
+  element.remove();
 });
 
+genre.textContent = "Драма";
+
+poster.style.backgroundImage = "url('img/bg.jpg')";
+
+movieList.innerHTML = "";
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+  movieList.innerHTML += `
+    <li class="promo__interactive-item">${i+1}) ${film}
+      <div class="delete"></div>
+    </li>
+  `;
+});
