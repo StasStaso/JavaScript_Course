369a2f40cd37bc898fa4b5a2f72740ab1f55cbb8
Stanislav Staso <stanislav.staso@charityhero.live>
2025-03-03 10:55:37 +0200

    js-step-react-2-modify-2

--- src/JS_Step_React_2_Practice/employers/src/components/app/app.js
+++ src/JS_Step_React_2_Practice/employers/src/components/app/app.js
@@ -6,6 +6,14 @@ import EmployeesList from "../employees-list/employees-list";
 import EmployeesAddForm from "../employees-add-form/employees-add-form";
 
 function App() {
+  const data = [
+    { name: "John S.", salary: 800, increase: false},
+    { name: "Alex M.", salary: 1800, increase: true},
+    { name: "Carl W.", salary: 3200, increase: false},
+    { name: "Carl A.", salary: 2500, increase: false},
+    { name: "Andr X.", salary: 1700, increase: false},
+  ];
+
   return (
     <div className="app">
       <AppInfo />
@@ -15,7 +23,7 @@ function App() {
         <AppFilter />
       </div>
 
-      <EmployeesList />
+      <EmployeesList data={data} />
       <EmployeesAddForm />
     </div>
   );
